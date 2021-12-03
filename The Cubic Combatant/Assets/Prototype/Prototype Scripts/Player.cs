263da6c51c238db3813166b098c9d654d2fcdf28
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-12-03 15:56:09 +0000

    Updated Player.cs
    Included rigidbody, bool and vector3 variables. Included a getcomponent function to get the player's rigidbody component. Implemented an inputvector for the player to move.

--- The Cubic Combatant/Assets/Prototype/Prototype Scripts/Player.cs
+++ The Cubic Combatant/Assets/Prototype/Prototype Scripts/Player.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 [RequireComponent (typeof (Rigidbody))] //if a rigidbody is missing, Unity attempts to add this type of component for you
 public class Player : MonoBehaviour
 {
+    private Rigidbody playerRB; //this is the Player's Rigidbody
+    private bool isJumped; //checks if the Player has jumped or not
+
+    private Vector3 inputVector; //this is a vector to where the Player move to
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB.GetComponent<Rigidbody>(); //gets the Rigidbody component
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        inputVector = new Vector3(Input.GetAxis("Horizontal"), playerRB.velocity.y, Input.GetAxis("Vertical"));
     }
 }
