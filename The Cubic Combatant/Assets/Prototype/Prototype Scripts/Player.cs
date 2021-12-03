7296abb5fc3849eb8c6261c7bd6c62d4e697da0f
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-12-03 15:45:17 +0000

    Updated Player.cs
    Added RequireComponent typeof RigidBody to include a Rigidbody if it doesn't exist on the Player object.

--- The Cubic Combatant/Assets/Prototype/Prototype Scripts/Player.cs
+++ The Cubic Combatant/Assets/Prototype/Prototype Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof (Rigidbody))] //if a rigidbody is missing, Unity attempts to add this type of component for you
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
