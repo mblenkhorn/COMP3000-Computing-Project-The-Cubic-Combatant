702b36eeb7b423dee464783492b94a9a3df7e5f6
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-12-19 22:43:36 +0000

    Updated HealthBar.cs
    Updated healthbar to animate health decreasing and included different colours to display the state of the health to notify player when health is or not critical.

--- The Cubic Combatant/Assets/Prototype/Prototype Scripts/HealthBar.cs
+++ The Cubic Combatant/Assets/Prototype/Prototype Scripts/HealthBar.cs
@@ -5,22 +5,52 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public int health = 8;
+    public Slider healthBarSlider;
 
-    public float fillSpeed = 0.5f;
+    private float currentHealth;
 
-    public Slider healthBar;
+    private float fillSpeed = 0.75f;
 
-    public void DecreaseHealth()
+    private Color satisHealthColour = Color.green;
+    private Color halfHealthColour = Color.yellow;
+    private Color nearDeathColour = Color.red;
+
+    public Image fillColour;
+
+
+    private void Start()
     {
-        healthBar.value -= fillSpeed * Time.deltaTime;
+        DecrementHealth(7f);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.J))
+        if(healthBarSlider.value > currentHealth)
         {
-            DecreaseHealth();
+            healthBarSlider.value -= fillSpeed * Time.deltaTime;
+        }
+
+        if (healthBarSlider.value <= 6f && healthBarSlider.value >= 4f)
+        {
+            fillColour.color = satisHealthColour;
+        }
+        else if (healthBarSlider.value <= 4f && healthBarSlider.value >= 2f)
+        {
+            fillColour.color = halfHealthColour;
+        }
+        else if(healthBarSlider.value <= 2f)
+        {
+            fillColour.color = nearDeathColour;
         }
     }
+
+
+    public void DecrementHealth(float newHealth)
+    {
+        currentHealth = healthBarSlider.value - newHealth;
+        Debug.Log(currentHealth);
+    }
+
+
+
 }
