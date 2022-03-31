ebf2dbe986ce06d092632fad14adba294f0b6b2d
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2022-03-31 02:47:36 +0100

    Edited UI.cs
    Added functionality for TextMeshPro.

--- The Cubic Combatant/Assets/Prototype/Prototype Scripts/UI.cs
+++ The Cubic Combatant/Assets/Prototype/Prototype Scripts/UI.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UI : MonoBehaviour
 {
-    public Text collectableText;
+    //public Text collectableText;
     public CollectableManager manager;
+
+    public TMP_Text collectableTextPro;
 
     public void UpdateText()
     {
-        collectableText.text = manager.getCollectable().ToString();
+        //collectableText.text = manager.getCollectable().ToString();
+
+        collectableTextPro.text = manager.getCollectable().ToString();
     }
 }
