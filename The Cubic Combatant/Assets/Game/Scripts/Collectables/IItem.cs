515de09be3706ca69c69cfade724619b020957dc
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2022-05-05 23:19:00 +0100

    Created IItem.cs
    Created an interface for all the items to get the value stored within the classes that implement IItem.

---
+++ The Cubic Combatant/Assets/Game/Scripts/Collectables/IItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IItem 
+{
+    public int getValue();
+
+    public void setValue(int valueToSet);
+}
