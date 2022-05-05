2f9e5fb2a569015be316e891e122ee5f0bde0892
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2022-05-05 23:15:09 +0100

    Created HealthPickup.cs
    Created HealthPickup for allowing the Player to collect health powerups.

---
+++ The Cubic Combatant/Assets/Game/Scripts/Collectables/Health/HealthPickup.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : Pickup
+{
+
+    [SerializeField]
+    private HealthManager manager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<HealthManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Collect();
+        }
+    }
+
+    public override void Collect()
+    {
+        manager.setValue(2f);
+        Destroy(gameObject);
+    }
+}
