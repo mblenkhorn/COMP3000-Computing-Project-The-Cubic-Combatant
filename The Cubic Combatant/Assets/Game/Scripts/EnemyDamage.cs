e2ef086aeb0ba7e3ff6cc6c0582ff62a2c113d5f
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2022-05-06 12:59:54 +0100

    Created EnemyDamage.cs
    Created a script to damage the player when he touches an enemy.

---
+++ The Cubic Combatant/Assets/Game/Scripts/EnemyDamage.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public HealthManager manager;
+
+    public bool isDamaged;
+
+    public Health health;
+
+    public KillPlayer playerDie;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !isDamaged)
+        {
+            manager.setValue(-2f);
+            isDamaged = true;
+            StartCoroutine(WaitForDamage());
+            isDamaged = false;
+        }
+
+        if(health.getHealth() == 0)
+        {
+            playerDie.Kill();
+        }
+    }
+
+    IEnumerator WaitForDamage()
+    {
+        yield return new WaitForSeconds(2f);
+    }
+}
